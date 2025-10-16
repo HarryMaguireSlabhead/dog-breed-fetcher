e0e91a3d06266d84b5710921c1894e680c7609df
Johnson Qin <johnsonqin0@gmail.com>
2025-10-16 16:18:09 -0400

    Task 4

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -25,7 +25,7 @@ public class CachingBreedFetcher implements BreedFetcher {
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         if (subBreedCache.containsKey(breed)) {
             return subBreedCache.get(breed);
         }
